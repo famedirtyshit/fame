0483771817c08d418edf61574f6e3aa707e43f55
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-09-26 01:02:58 +0700

    [Add]Tester

---
+++ src/scenes/Tester.js
@@ -0,0 +1,17 @@
+class Tester extends Phaser.Scene {
+    constructor(){
+        super({
+            key: 'Tester'
+        })
+    }
+    preload(){
+
+    }
+    create(){
+
+    }
+    update(time, delta){
+        
+    }
+}
+export default Tester
